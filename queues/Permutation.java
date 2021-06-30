37a3a70b9b26b4a89befd51d01b37ba9ec161de8
gbudau <54190400+gbudau@users.noreply.github.com>
2021-07-01 00:29:04 +0200

    Add Permutation
    Added Permutation client

---
+++ queues/Permutation.java
@@ -0,0 +1,31 @@
+import edu.princeton.cs.algs4.StdIn;
+
+public class Permutation
+{
+    public static void main(String[] args)
+    {
+        if (args.length != 1)
+        {
+            System.out.println("Usage: java Permutation k");
+            return;
+        }
+        int k = Integer.parseInt(args[0]);
+        if (k <= 0)
+        {
+            System.out.println("Error: k must be a positive number");
+            return;
+        }
+        RandomizedQueue<String> rq = new RandomizedQueue<String>();
+        String s;
+        while (!StdIn.isEmpty())
+        {
+            s = StdIn.readString();
+            rq.enqueue(s);
+        }
+        while (!rq.isEmpty() && k-- != 0)
+        {
+            s = rq.dequeue();
+            System.out.println(s);
+        }
+    }
+}
--- queues/RandomizedQueue.java
+++ queues/RandomizedQueue.java
@@ -3,115 +3,143 @@ import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomizedQueue<Item> implements Iterable<Item>
 {
-	private Item[] data;
-	private int size;
+    private Item[] data;
+    private int size;
 
-	// construct an empty randomized queue
-	public RandomizedQueue()
-	{
-		data = null;
-		size = 0;
-	}
+    // construct an empty randomized queue
+    public RandomizedQueue()
+    {
+        data = null;
+        size = 0;
+    }
 
-	// is the randomized queue empty?
-	public boolean isEmpty()
-	{
-		return size == 0;
-	}
+    // is the randomized queue empty?
+    public boolean isEmpty()
+    {
+        return size == 0;
+    }
 
-	// return the number of items on the randomized queue
-	public int size()
-	{
-		return size;
-	}
+    // return the number of items on the randomized queue
+    public int size()
+    {
+        return size;
+    }
 
 
-	// add the item
-	public void enqueue(Item item)
-	{
-		if (item == null)
-		{
-			throw new IllegalArgumentException(
-					"Error: RandomizedQueue.enqueue(): illegal argument null");
-		}
-		if (isEmpty() || size == data.length)
-		{
-			resize(isEmpty() ? 1 : 2 * data.length);
-		}
-		data[size++] = item;
-	}
+    // add the item
+    public void enqueue(Item item)
+    {
+        if (item == null)
+        {
+            throw new IllegalArgumentException(
+                    "Error: RandomizedQueue.enqueue(): illegal argument null");
+        }
+        if (isEmpty() || size == data.length)
+        {
+            resize(isEmpty() ? 1 : 2 * data.length);
+        }
+        if (isEmpty())
+        {
+            data[size++] = item;
+        }
+        else
+        {
+            final int randomIndex = StdRandom.uniform(size);
+            Item temp = data[randomIndex];
+            data[randomIndex] = item;
+            data[size++] = temp;
+        }
+    }
 
-	// resize the randomized queue
-	private void resize(int capacity)
-	{
-		Item[] copy = (Item[]) new Object[capacity];
-		for (int i = 0; i < size; i++)
-		{
-			copy[i] = data[i];
-		}
-		data = copy;
-	}
+    // resize the randomized queue
+    private void resize(int capacity)
+    {
+        Item[] copy = (Item[]) new Object[capacity];
+        for (int i = 0; i < size; i++)
+        {
+            copy[i] = data[i];
+        }
+        data = copy;
+    }
 
-	// remove  and return a random item
-	public Item dequeue()
-	{
-		if (isEmpty())
-		{
-			throw new java.util.NoSuchElementException(
-					"Error: RandomizedQueue.dequeue(): no such element");
-		}
-		Item item = data[--size];
-		data[size] = null;
-		if (!isEmpty() && size == data.length / 4)
-		{
-			resize(data.length / 2);
-		}
-		return item;
-	}
-	
-	// return a random item (but do not remove it)
-	public Item sample()
-	{
-		if (isEmpty())
-		{
-			throw new java.util.NoSuchElementException(
-					"Error: RandomizedQueue.sample(): no such element");
-		}
-		final int index = StdRandom.uniform(size - 1);
-		return data[index];
-	}
+    // remove  and return a random item
+    public Item dequeue()
+    {
+        if (isEmpty())
+        {
+            throw new java.util.NoSuchElementException(
+                    "Error: RandomizedQueue.dequeue(): no such element");
+        }
+        final int randomIndex = StdRandom.uniform(size);
+        Item item = data[randomIndex];
+        data[randomIndex] = data[--size];
+        item = data[size];
+        data[size] = null;
+        if (!isEmpty() && size == data.length / 4)
+        {
+            resize(data.length / 2);
+        }
+        return item;
+    }
+    
+    // return a random item (but do not remove it)
+    public Item sample()
+    {
+        if (isEmpty())
+        {
+            throw new java.util.NoSuchElementException(
+                    "Error: RandomizedQueue.sample(): no such element");
+        }
+        final int index = StdRandom.uniform(size - 1);
+        return data[index];
+    }
 
-	// return an independent iterator over items in random order
-	public Iterator<Item> iterator()
-	{
-		return new RandomizedQueueIterator();
-	}
+    // return an independent iterator over items in random order
+    public Iterator<Item> iterator()
+    {
+        return new RandomizedQueueIterator();
+    }
 
-	private class RandomizedQueueIterator implements Iterator<Item>
-	{
-		private int i = size;
+    private class RandomizedQueueIterator implements Iterator<Item>
+    {
+        private int start;
+        private int index;
 
-		public boolean hasNext()
-		{
-			return i > 0;
-		}
+        RandomizedQueueIterator()
+        {
+            start = StdRandom.uniform(size);
+            index = start;
+        }
 
-		public void remove()
-		{
-			throw new UnsupportedOperationException(
-					"Error: RandomizedQueue.iterator.remove()");
-		}
+        public boolean hasNext()
+        {
+            if (isEmpty())
+            {
+                return false;
+            }
+            else
+            {
+                return (index + 1) % size != start;
+            }
+        }
 
-		public Item next()
-		{
-			return data[--i];
-		}
-	}
+        public void remove()
+        {
+            throw new UnsupportedOperationException(
+                    "Error: RandomizedQueue.iterator.remove()");
+        }
 
-	// unit testing
-	public static void main(String[] args)
-	{
-		RandomizedQueue<Integer> rq = new RandomizedQueue<Integer>();
+        public Item next()
+        {
+            index = (index + 1) % size;
+            return data[index];
+        }
+    }
+
+    // unit testing
+    public static void main(String[] args)
+    {
+        RandomizedQueue<Integer> rq = new RandomizedQueue<Integer>();
         System.out.println("RandomizedQueue is empty?: " + rq.isEmpty());
         System.out.println("RandomizedQueue size: " + rq.size());
         System.out.println("Adding 2 to the RandomizedQueue");
@@ -121,13 +149,31 @@ public class RandomizedQueue<Item> implements Iterable<Item>
         System.out.println("Adding 1 to the RandomizedQueue");
         rq.enqueue(1);
         System.out.println("RandomizedQueue size: " + rq.size());
+        System.out.println("Adding 10 random integers to the RandomizedQueue");
+        for (int i = 0; i < 10; ++i)
+        {
+            rq.enqueue(StdRandom.uniform(Integer.MAX_VALUE));
+        }
+        System.out.println("RandomizedQueue size: " + rq.size());
+        System.out.println();
         System.out.println("Printing the RandomizedQueue using iterators");
         for (int i : rq)
+        {
             System.out.println(i);
-        System.out.println("Removing one item from the RandomizedQueue: "
-                + rq.dequeue());
-        System.out.println("Removing one item from the RandomizedQueue: "
-                + rq.dequeue());
+        }
+        System.out.println();
+        System.out.println("Printing the RandomizedQueue using iterators");
+        for (int i : rq)
+        {
+            System.out.println(i);
+        }
+        System.out.println();
+        System.out.println("Removing all the items from the RandomizedQueue");
+        while (!rq.isEmpty())
+        {
+            System.out.println("Removing one item from the RandomizedQueue: "
+                    + rq.dequeue());
+        }
 
-	}
+    }
 }
