e236b425229acd13631910d8f63f5fb0a878091d
gbudau <54190400+gbudau@users.noreply.github.com>
2021-07-10 20:49:54 +0200

    Add Board constructor
    Added Board constructor, toString and equals method

--- 8puzzle/Board.java
+++ 8puzzle/Board.java
@@ -1,48 +1,119 @@
+import java.util.Arrays;
+import edu.princeton.cs.algs4.In;
+
 public class Board {
+    private final int N;          // number of rows/columns
+    private final int[][] tiles;  // N-by-N array
 
-	// create a board from an n-by-n array of tiles,
-	// where tiles[row][col] = tile at (row, col)
-	public Board(int[][] tiles)
-	{
-	}
+    // create a board from an n-by-n array of tiles,
+    // where tiles[row][col] = tile at (row, col)
+    public Board(int[][] tiles)
+    {
+        if (tiles == null)
+        {
+            throw new IllegalArgumentException(
+                    "Error: Board: Invalid argument: null");
+        }
+        N = tiles.length;
+        this.tiles = new int[N][N];
+        for (int row = 0; row < N; ++row)
+        {
+            if (tiles.length != N)
+            {
+                throw new IllegalArgumentException(
+                        "Error: Board: Invalid array size");
+            }
+            for (int col = 0; col < N; ++col)
+            {
+                this.tiles[row][col] = tiles[row][col];
+            }
+        }
+    }
 
-	// string representation of this board
-	public String toString()
-	{
-	}
+    // string representation of this board
+    public String toString()
+    {
+        StringBuilder s = new StringBuilder();
+        s.append(N + "\n");
+        for (int row = 0; row < N; ++row)
+        {
+            for (int col = 0; col < N; ++col)
+            {
+                s.append(String.format("%2d ", tiles[row][col]));
+            }
+            s.append("\n");
+        }
+        return s.toString();
+    }
 
-	// board dimension n
-	public int dimension()
-	{
-	}
+    // board dimension n
+    public int dimension()
+    {
+        return N;
+    }
 
-	// sum of Manhattan distances between tiles and goal
-	public int manhattan()
-	{
-	}
+    // sum of Manhattan distances between tiles and goal
+    public int manhattan()
+    {
+        // TODO
+        return 0;
+    }
 
-	// is this board the goal board?
-	public boolean isGoal()
-	{
-	}
+    // is this board the goal board?
+    public boolean isGoal()
+    {
+        // TODO
+        return true;
+    }
 
-	// does this board equal y?
-	public boolean equals(Object y)
-	{
-	}
+    // does this board equal y?
+    public boolean equals(Object aThat)
+    {
+        if (this == aThat)
+        {
+            return true;
+        }
+        if (aThat == null)
+        {
+            return false;
+        }
+        if (this.getClass() != aThat.getClass())
+        {
+            return false;
+        }
+        Board that = (Board) aThat;
+        return N == that.N && Arrays.deepEquals(tiles, that.tiles);
+    }
 
-	// all neighboring boards
-	public Iterable<Board> neighbors()
-	{
-	}
+    // all neighboring boards
+    public Iterable<Board> neighbors()
+    {
+        // TODO
+        return null;
+    }
 
-	// a board that is obtained by excanging any pair of tiles
-	public Board twin()
-	{
-	}
+    // a board that is obtained by exchanging any pair of tiles
+    public Board twin()
+    {
+        // TODO
+        return null;
+    }
 
-	// unit testing
-	public static void main(String[] args)
-	{
-	}
+    // unit testing
+    public static void main(String[] args)
+    {
+        // create initial board from file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] tiles = new int[n][n];
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                tiles[i][j] = in.readInt();
+            }
+        }
+        Board initial = new Board(tiles);
+        System.out.println(initial.toString());
+    }
 }
