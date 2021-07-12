e4d81149016e5cfb9b506315316b4dc75719a729
gbudau <54190400+gbudau@users.noreply.github.com>
2021-07-12 21:25:09 +0200

    Add SearchNode to 8puzzle Solver
    Added the SearchNode nested class that stores the nodes
    searched so far

--- 8puzzle/Solver.java
+++ 8puzzle/Solver.java
@@ -1,5 +1,4 @@
 import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.MinPQ;
 
@@ -22,6 +21,45 @@ public class Solver {
     // sequence of boards in a shortest solution; null if unsolvable
     public Iterable<Board> solution()
     {
+    }
+
+    private class SearchNode implements Comparable<SearchNode>
+    {
+        private SearchNode prev;
+        private int moves;
+        private int priority;
+        private Board board;
+
+        public SearchNode(Board board, int moves, SearchNode prev)
+        {
+            this.board = board;
+            this.moves = moves;
+            this.prev = prev;
+            priority = this.board.manhattan();
+        }
+
+        public int compareTo(SearchNode that)
+        {
+            final int priority_this = priority + moves;
+            final int priority_that = that.priority + that.moves;
+            if (priority_this < priority_that)
+            {
+                return -1;
+            }
+            if (priority_this > priority_that)
+            {
+                return 1;
+            }
+            if (priority < that.priority)
+            {
+                return -1;
+            }
+            if (priority > that.priority)
+            {
+                return 1;
+            }
+            return 0;
+        }
     }
 
     // test client
